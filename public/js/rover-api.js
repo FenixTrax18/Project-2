4f789f12c1cad39b36b04555f4955906692c2565
Cheryl Daniels <cheryld433@gmail.com>
2020-06-19 21:21:15 -0400

    added rover html and jsj files

---
+++ public/js/rover-api.js
@@ -0,0 +1,15 @@
+var queryURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=Lw59dX1hoc67rrcErxzay8oTsrx0Cw2qZBdKTusr";
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+  .then(function (response) {
+    console.log(queryURL);
+     for(var i = 0; i < 4; i++) {
+         console.log("#" + i);
+         var id = "#" + i;
+         console.log(id)
+       $(id).attr("src", response.photos[i].img_src);
+       console.log("Image Source:", response.photos[i].img_src); 
+    };
+});
