1a47829764461c6a8b6f661b5fae9744e732e0f0
AFeese <58886725+AFeese@users.noreply.github.com>
2020-06-21 19:39:51 -0400

    added back to top button, appears after 20px of scroll toward bottom.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
+  }).then(function (response) {
     console.log(queryURL);
     console.log(response);
     $(".url").html("<img src=" + response.url + ">" + "</img>");
@@ -14,3 +14,20 @@ $(document).ready(() => {
     console.log(response.explanation);
   });
 });
+
+//Scroll to top button
+var mybutton = document.getElementById("topBtn");
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
