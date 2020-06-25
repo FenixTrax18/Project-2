005cab71829aa31e32f24acea54a7f32a4d90989
Jon Christie <jonpchristie@gmail.com>
2020-06-25 00:26:14 -0400

    finalizing touches

--- TESTROUTES/htmlroutes.js
+++
@@ -1,9 +0,0 @@
-var path = require(“path”);
-
-
-module.exports = function(app) {
-    app.get(“/”, function(req,res){
-        res.sendFile(path.join(_dirname, “public\ TESTROUTES\ notification.js”));
-    });
-};
-// DO I need to add more to this. This is just to load the HTML page:
