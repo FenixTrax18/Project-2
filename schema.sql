e4c916f2eec8f5dc3e63028459c9ae8dbeb2c9a7
Jonathan P Christie <jonpchristie@gmail.com>
2020-06-24 22:03:45 -0400

    table name='users' & added JAWSDB schema

--- schema.sql
+++ schema.sql
@@ -5,7 +5,23 @@ CREATE DATABASE loginInfo;
 
 USE loginInfo;
 
-CREATE TABLE IF NOT EXISTS signUp(
+CREATE TABLE IF NOT EXISTS users(
+email VARCHAR(100) NOT NULL,
+password VARCHAR(100) NOT NULL,
+notification BOOLEAN NOT NULL DEFAULT false);
+
+SHOW TABLES
+
+
+
+--HEROKU DATABASE
+DROP DATABASE IF EXISTS gzbiflzbgaf8hexk;
+
+CREATE DATABASE gzbiflzbgaf8hexk;
+
+USE gzbiflzbgaf8hexk;
+
+CREATE TABLE IF NOT EXISTS users(
 email VARCHAR(100) NOT NULL,
 password VARCHAR(100) NOT NULL,
 notification BOOLEAN NOT NULL DEFAULT false);
